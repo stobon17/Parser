00b4339cc9b527378435b9a9b3cc49ec28a41f9d
stobon <stobon@ufl.edu>
2020-12-11 13:07:34 -0500

    added header

---
+++ Lexical.h
@@ -0,0 +1,13 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include "tree.h"
+#include <regex>
+
+using namespace std;
+
+
+//Functions
+vector<Token> LexVec(std::string file);
